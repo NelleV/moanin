e38b197d2894d9acf27aafa513e6b7c9ee199225
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2019-08-13 14:18:37 +0200

    Renaming function plot_centroids/plot_genes

--- tests/testthat/test-visualization.R
+++ tests/testthat/test-visualization.R
@@ -16,16 +16,19 @@ test_that("visualization::plot_centroid_individual", {
 })
 
 
-test_that("visualization::plot_centroids", {
+test_that("visualization::plot_splines_data", {
     data(shoemaker2015)
     data = as.matrix(shoemaker2015$data[5, ])
     meta = shoemaker2015$meta
 
     splines_model = create_splines_model(meta)
 
-    expect_silent(plot_centroids(data, splines_model))
-    expect_silent(plot_centroids(data, splines_model, smooth=TRUE))
-    expect_error(plot_centroids(data, splines_model, smooth=TRUE, mfrow=c(1,1)))
-    expect_silent(plot_centroids(data, splines_model, smooth=TRUE, mfrow=c(1,1), legend=FALSE))
+    expect_silent(plot_splines_data(data, splines_model))
+    expect_silent(plot_splines_data(data, splines_model, smooth=TRUE))
+    expect_error(plot_splines_data(data, splines_model,
+				   smooth=TRUE, mfrow=c(1, 1)))
+    expect_silent(plot_splines_data(data, splines_model,
+				    smooth=TRUE, mfrow=c(1, 1),
+				    legend=FALSE))
 
 })
