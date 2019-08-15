515007d95a646da7dab1ba7b866491e08b2f186a
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2019-08-15 14:56:14 +0200

    ENH Contrasts can now be provided as either limma contrasts or vector of strings
    closes #9

--- R/validation.R
+++ R/validation.R
@@ -84,3 +84,51 @@ check_is_2d = function(X){
 	stop("Data is expected to be 2D. No dimension found.")
     }
 }
+
+
+#' Check is contrasts
+#'
+#' Will check that the contrasts provided are indeed contrasts. contrasts are
+#'  expected to be either a vector of string or a matrix containing the
+#'  contrasts coefficients.
+#'  
+#' If a vector of string is provided, the function will call
+#'  limma::makeContrast in order to obtain the contrasts coefficients.
+#'
+#' If a contrasts matrix is provided, it will perform a number of checks on
+#' the contrasts matrix to make sure it contains the number of rows expected,
+#' and that each contrast indeed sums to 0.
+#'
+#' @keywords internal
+is_contrasts = function(contrasts, meta){
+    if(is.vector(contrasts)){
+	# XXX Should we add more tests here in order to provide meaningful
+	# error messages?
+	contrasts = limma::makeContrasts(
+	    contrasts=contrasts,
+	    levels=levels(meta$Group))
+    }else{
+	# Basic checks to make sure we have contrasts
+	dim_contrasts = dim(contrasts)
+	if(is.null(dim_contrasts)){
+	    msg = "Contrasts need to be either a vector of string or a 2D matrix"
+	    stop(msg)
+	}
+
+	n_groups = length(levels(meta$Group))
+	if(dim(contrasts)[1] != n_groups){
+	    msg = paste(
+		"When contrasts provided is a matrix, it should contain the",
+		"same number of rows as the number of groups. Provided:",
+		dim(contrats[1]), "expected:", n_groups)
+	    stop(msg)
+	}
+
+	if(any(colSums(contrasts) != 0)){
+	    msg = paste(
+		"When contrasts provided is matrix, all columns should sum to 0.")
+	    stop(msg)
+	}
+    }
+    return(contrasts) 
+}
