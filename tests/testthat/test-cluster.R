e5af0f94e39d2a716fa777a35ef37c5661024082
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2020-07-03 18:53:06 -0700

    tests pass

--- tests/testthat/test-cluster.R
+++ tests/testthat/test-cluster.R
@@ -1,12 +1,12 @@
 library(moanin)
 library(timecoursedata)
+data(shoemaker2015)
 library(testthat)
 
 
 context("moanin::cluster.R")
 
 test_that("cluster::splines_kmeans", {
-    data(shoemaker2015)
     data = shoemaker2015$data
     meta = shoemaker2015$meta
     random_seed = 42
@@ -19,7 +19,6 @@ test_that("cluster::splines_kmeans", {
 
 
 test_that("cluster::splines_kmeans_score_and_label", {
-    data(shoemaker2015)
     data = shoemaker2015$data
     meta = shoemaker2015$meta
     random_seed = 42
