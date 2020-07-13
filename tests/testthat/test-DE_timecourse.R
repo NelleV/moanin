3f85f52ea2b1f2945eb9fc351b0d854b8ebbf067
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2020-07-13 14:34:19 -0700

    pass test_DE_timepoints.R

--- tests/testthat/test-DE_timecourse.R
+++ tests/testthat/test-DE_timecourse.R
@@ -1,42 +1,33 @@
 library(moanin)
 library(limma)
-library(timecoursedata)
-data(shoemaker2015)
 
 context("moanin::de_analysis.R")
 
 test_that("time-course DE analysis", {
-    data = shoemaker2015$data
-    meta = shoemaker2015$meta
 
-    moanin_model = create_moanin_model(meta)
-
-    # Limit ourselves to the top 5000 genes
-    data = data[1:5000, ]
+    moanin_model = create_moanin_model(data=testData,meta=testMeta)
 
     # Do only one contrast
     contrast_formulas = c("K-M")
     contrast = limma::makeContrasts(
 	contrasts=contrast_formulas,
-	levels=levels(meta$Group))
+	    levels=levels(droplevels(testMeta$Group)))
 
-    expect_silent(moanin::DE_timecourse(
-	as.matrix(data), moanin_model, contrasts=contrast,
-	use_voom_weights=FALSE))
+    expect_silent(moanin::DE_timecourse(moanin_model, contrasts=contrast,
+	    use_voom_weights=FALSE))
 
     # Now test with several contrasts
     contrast_formulas = c("K-M", "C-M")
     contrasts = limma::makeContrasts(
-	contrasts=contrast_formulas,
-	levels=levels(meta$Group))
+	    contrasts=contrast_formulas,
+	    levels=levels(droplevels(testMeta$Group)))
 
-    expect_silent(moanin::DE_timecourse(
-    	as.matrix(data), moanin_model, contrasts=contrast,
-	use_voom_weights=FALSE))
+    expect_silent(moanin::DE_timecourse(moanin_model, contrasts=contrast,
+	    use_voom_weights=FALSE))
     
     # Check that the names of the columns make sense.
     de_results = moanin::DE_timecourse(
-	as.matrix(data), moanin_model, contrasts=contrast,
-	use_voom_weights=FALSE)
+	    moanin_model, contrasts=contrast,
+	    use_voom_weights=FALSE)
 
 })
