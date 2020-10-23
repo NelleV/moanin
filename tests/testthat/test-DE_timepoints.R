273cb19477704a92bc6e822e655051db9a9e4eef
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2020-10-23 14:38:55 -0700

    update tests deal with different names

--- tests/testthat/test-DE_timepoints.R
+++ tests/testthat/test-DE_timepoints.R
@@ -5,13 +5,31 @@ library(limma)
 context("moanin::DE_timepoints.R")
 
 test_that("DE_timepoints::create_timepoints_contrasts", {
-
-    moanin_model = create_moanin_model(data=testData,meta=testMeta)
-    expect_silent(create_timepoints_contrasts( moanin_model,"M", "C"))
+    #change group variable to check that works everywhere
+    colnames(testMeta)<-c("Index","condition","replicate","time")
+    
+    moanin_model = create_moanin_model(data=testData,meta=testMeta,
+        group_variable_name="condition",
+        time_variable_name="time")
+    expect_silent(contrasts<-create_timepoints_contrasts( moanin_model,"M", "C"))
 
     # Now drop timepoint 6 in condition C. That should raise a warning
-    mask = !((testMeta$Group == "C") & (testMeta$Timepoint == 6))
+    mask = !((testMeta$condition == "C") & (testMeta$time == 6))
     msg = paste0("timepoint 6 is missing in condition C")
-    expect_warning(create_timepoints_contrasts(moanin_model[,mask],"M", "C"), msg)
+    expect_warning(create_timepoints_contrasts(moanin_model[,mask],
+        "M", "C"), msg)
+        
+    expect_silent( DE_timepoints(moanin_model,
+         contrasts=contrasts, use_voom_weights=FALSE))
+    # Create a second moanin model with counts
+    moanin_model_counts = create_moanin_model(
+        data=round(exp(testData)), meta=testMeta,
+        log_transform=TRUE,
+        group_variable_name="condition",
+        time_variable_name="time")
+    expect_silent( DE_timepoints(moanin_model_counts,
+          contrasts=contrasts, use_voom_weights=TRUE))
+    
+         
 
 })
