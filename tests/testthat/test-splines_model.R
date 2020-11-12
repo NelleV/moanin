c6fa2b3ba97846db52b1d15f43cb787f9c4fed0b
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2020-11-11 16:13:14 -0800

    add test function for discont_basis

--- tests/testthat/test-splines_model.R
+++ tests/testthat/test-splines_model.R
@@ -15,3 +15,15 @@ test_that("moanin_model::create_moanin_model", {
     expect_error(create_moanin_model(data=testData,meta=testMeta, 
                        basis_matrix=basis, spline_formula=formula))
 })
+test_that("moanin_model::discont_basis",{
+    #test function
+    x<-seq(0,10,length=100)
+    basis<-discont_basis(x,discont_point=3, dfPre=3, dfPost=4, intercept=TRUE)
+     
+    # Use it in a moanin_model object instead of ns/bs:
+    moanin <- create_moanin_model(data=testData, meta=testMeta,
+        spline_formula=~Group:discont_basis(Timepoint,dfPre=3,
+            dfPost=3,discont=20,intercept=TRUE)+0,
+        df=6)
+    
+})
